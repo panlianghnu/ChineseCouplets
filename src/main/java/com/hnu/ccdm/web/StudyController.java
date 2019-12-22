bb2bc2c22fca3237fa749912374ef237e28f9fc2
xzc <1335718228@qq.com>
2019-12-22 21:04:03 +0800

    sign

--- src/main/java/com/hnu/ccdm/web/StudyController.java
+++ src/main/java/com/hnu/ccdm/web/StudyController.java
@@ -1,6 +1,7 @@
 package com.hnu.ccdm.web;
 
 import com.hnu.ccdm.entity.Charpter;
+import com.hnu.ccdm.entity.Hotsearch;
 import com.hnu.ccdm.entity.Study;
 import com.hnu.ccdm.service.CharpterService;
 import com.hnu.ccdm.service.StudyService;
@@ -9,7 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Controller
@@ -50,7 +54,20 @@ public class StudyController {
                 toback.add(x);
             }
         }
+        Collections.sort(toback, new Comparator<Charpter>() {
+            @Override
+            public int compare(Charpter o1, Charpter o2) {
+
+                int diff=Integer.valueOf(o1.getCharpternumber())-Integer.valueOf(o2.getCharpternumber());
+                if (diff>0) {
+                    return 1;
+                }
+                else if (diff<0){
+                    return -1;
+                }
+                return 0;
+            }
+        });
         return toback;
     }
-
 }
